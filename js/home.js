62a780718580530722c8fc619a65f2b247fac8d7
Michael Lyons <mdl0394@gmail.com>
2015-06-04 23:14:27 -0700

    Added highlight js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,31 @@
 ---
 # Frontmatter
 ---
+
+function setupGallery() {
+    $(".galleryimage").click(function( e ) {
+        $(".overlay").show();
+        $("#overlay_image").attr("src", $(e.target).attr("src") );
+    })
+
+    $(".overlay").click(function() {
+        $(".overlay").hide();
+    })
+}
+
+function changeColor() {
+    // $("body").addClass("color1");
+}
+
+$(document).ready(function() {
+    setupGallery();
+    changeColor();
+    $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
+
+$(document).on('page:change',  function() {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
+$(document).on('page:restore', function() {
+  $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});
